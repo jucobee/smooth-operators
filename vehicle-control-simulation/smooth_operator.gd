bd655738797cf14af2eb3f413801e098a4ad1107
jucobee <jucobee@gmail.com>
2024-05-29 15:32:55 -0700

    added controller node

--- vehicle-control-simulation/smooth_operator.gd
+++ vehicle-control-simulation/smooth_operator.gd
@@ -3,6 +3,10 @@ extends CharacterBody3D
 var gravity = 9.8
 var speed = 1000
 
+@export var controller: Node
+@export var cd: float
+@export var cr: float
+
 func _ready():
 	pass
 
@@ -11,3 +15,7 @@ func _physics_process(delta):
 	velocity.y -= gravity * delta
 	
 	move_and_slide()
+
+
+func _on_controller_power_output():
+	pass # Replace with function body.
