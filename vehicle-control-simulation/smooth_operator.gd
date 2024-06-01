3309a145cf14bfb69fd3d101552baf208dda0118
Divyansh Jain <divyanshrajeshjain@gmail.com>
2024-05-31 23:33:30 -0700

    Added Velocity Calculation Given Force Input:

--- vehicle-control-simulation/smooth_operator.gd
+++ vehicle-control-simulation/smooth_operator.gd
@@ -2,17 +2,22 @@ extends CharacterBody3D
 
 var gravity = 9.8
 var speed = 1000
+var force = 100
 
 @export var controller: Node
 @export var cd: float
 @export var cr: float
+@export var mass: float
+@export var frontal_area: float
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	velocity = Vector3.FORWARD * speed * delta
+	#velocity = Vector3.FORWARD * speed * delta
 	velocity.y -= gravity * delta
+	velocity = Vector3.FORWARD * sqrt(2 * (force - (cr * mass * gravity))/(cd * 1.3 * frontal_area))
+	
 	
 	move_and_slide()
 
